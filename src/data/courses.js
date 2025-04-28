bae587f7f8e93dfb201f1417835434ea2705223d
Rohithvicky <vickyrohith70@gmail.com>
2025-04-28 11:04:37 +0530

    removed courdetails,CourseList

---
+++ src/data/courses.js
@@ -0,0 +1,16 @@
+export const courses = [
+  {
+    id: 1,
+    title: 'Java Programming',
+    description: 'Learn Java programming basics.',
+    instructor: 'Prof. Johnson',
+    enrolled: true,
+  },
+  {
+    id: 2,
+    title: 'Foundations of Data Science',
+    description: 'Introduction to data science concepts.',
+    instructor: 'Dr. Patel',
+    enrolled: true,
+  },
+];
